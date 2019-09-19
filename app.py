6789c87bac4077f66788ac4a39f5e2939dfabe36
lucifier123 <lucifier1@live.com>
2019-09-20 01:24:33 +0530

    new

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route("/home")
+def home():
+    return render_template('home.html')
+@app.route("/contact")
+def contact():
+    return "WELCOME TO MY CONTACT PAGE"
+if(__name__=="__main__"):
+    app.run(debug=True)
